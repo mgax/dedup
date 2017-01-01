cacd3534f1e7919542126630aa56402128497460
Alex Morega <alex@grep.ro>
2017-01-01 15:14:34 +0200

    split files in blocks

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,63 @@ fn main() {
 }
 
 struct Store {
-    blocks: HashMap<String, Vec<u8>>,
+    block_size: usize,
+    blocks: HashMap<Vec<u8>, Vec<u8>>,
+    files: HashMap<String, Vec<Vec<u8>>>,
 }
 
 impl Store {
-    pub fn new() -> Store {
-        Store{blocks: HashMap::new()}
+    pub fn new(block_size: usize) -> Store {
+        Store{
+            block_size: block_size,
+            blocks: HashMap::new(),
+            files: HashMap::new(),
+        }
     }
 
     pub fn save(&mut self, key: &str, data: &[u8]) {
-        self.blocks.insert(key.to_string(), data.to_vec());
+        let mut block_keys: Vec<Vec<u8>> = Vec::new();
+        let mut start = 0;
+        let mut size = 1;
+
+        loop {
+            if size == self.block_size {
+                let block = data[start .. start + size].to_vec();
+                let block_key = block.clone();
+                self.blocks.insert(block_key.clone(), block);
+                block_keys.push(block_key);
+                start += size;
+                size = 0;
+            }
+            if start + size == data.len() {
+                if size > 0 {
+                    let block = data[start .. start + size].to_vec();
+                    let block_key = block.clone();
+                    self.blocks.insert(block_key.clone(), block);
+                    block_keys.push(block_key);
+                    start += size;
+                    size = 0;
+                }
+                break;
+            }
+
+            size += 1;
+            assert!(size <= self.block_size);
+            assert!(start + size <= data.len());
+        }
+        assert!(size == 0);
+        assert!(start == data.len());
+
+        self.files.insert(key.to_string(), block_keys);
     }
 
     pub fn load(&self, key: &str) -> Vec<u8> {
-        self.blocks.get(key).unwrap().to_vec()
+        let mut out: Vec<u8> = Vec::new();
+        for block_key in self.files.get(key).unwrap().iter() {
+            let block = self.blocks.get(block_key).unwrap();
+            out.extend(block.as_slice());
+        }
+        return out;
     }
 }
 
@@ -26,9 +69,20 @@ impl Store {
 mod tests {
     #[test]
     fn single_small_file() {
-        let mut store = super::Store::new();
+        let mut store = super::Store::new(4);
         let fox = "the quick brown fox jumps over the lazy dog".as_bytes();
         store.save("fox", fox);
         assert_eq!(store.load("fox"), fox);
+    }
+
+    #[test]
+    fn two_small_files() {
+        let mut store = super::Store::new(4);
+        let fox_one = "the quick brown fox jumps over the lazy dog".as_bytes();
+        let fox_two = "the quack brewn fox jumped over lazy dog".as_bytes();
+        store.save("fox_one", fox_one);
+        store.save("fox_two", fox_two);
+        assert_eq!(store.load("fox_one"), fox_one);
+        assert_eq!(store.load("fox_two"), fox_two);
     }
 }
