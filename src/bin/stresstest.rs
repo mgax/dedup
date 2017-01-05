687fbe153f7af890cc6800f270e22af890de96e9
Alex Morega <alex@grep.ro>
2017-01-05 21:20:12 +0200

    grab the stdout stream

--- src/bin/stresstest.rs
+++ src/bin/stresstest.rs
@@ -4,7 +4,7 @@ extern crate dedup;
 
 use std::io;
 use std::io::prelude::*;
-use std::process::Command;
+use std::process::{Command, Stdio};
 use std::collections::HashMap;
 use regex::Regex;
 use crypto::digest::Digest;
@@ -27,15 +27,22 @@ fn main() {
         let uline = line.unwrap();
         let cap = re.captures(&uline).unwrap();
         let (name, cmd) = (&cap[1], &cap[2]);
-        let output = Command::new("sh").arg("-c").arg(cmd).output().unwrap();
-        let stats = store.save(name, &output.stdout);
+        let mut child =
+            Command::new("sh").arg("-c").arg(cmd)
+            .stdout(Stdio::piped())
+            .spawn()
+            .unwrap();
+        let mut data = vec!();
+        child.stdout.take().unwrap().read_to_end(&mut data).unwrap();
+        assert!(child.wait().unwrap().success());
+        let stats = store.save(name, &data);
         println!("{:24} {:12} / {:<6} {:12} / {:<6} fp={}",
             name,
             stats.new_bytes, stats.new_blocks,
             stats.dup_bytes, stats.dup_blocks,
             stats.roll_false,
         );
-        hashes.insert(name.to_string(), sha256(&output.stdout));
+        hashes.insert(name.to_string(), sha256(&data));
     }
     for (name, hash) in &hashes {
         let stored_hash = sha256(&store.load(name));
